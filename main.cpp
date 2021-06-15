c11c5209d395813e7b7f4504a33688ffaf78a35d
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-06-15 17:54:33 -0300

    functions fseek fgets #observations #file content

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ int main(){
         return(0);
     }
 
+    fseek(arquivo, sizeof(int), SEEK_SET);
+    fgets(Vet, 6664, arquivo);
     result = fread (&Vet[0], sizeof(int), 100, arquivo);
     printf("Nro de elementos lidos: %d\n", result);
 
@@ -21,4 +23,20 @@ int main(){
     printf("%lf\n", Vet[i]);
   
     fclose(arquivo);
+
+    /**
+     * CONTEUDO DO AQUIVO *
+    0000000 0864 2306 ef77 bdbf bfef efbd bdbf 7927
+    0000010 bfef efbd 1a08 0000 0000 bfef efbd bdbf
+    0000020 7745 bfef efbd bdbf bfef 00bd          
+    000002c
+    
+    6664 = 1A08
+
+    Encontrar o 1A08, rotacionar ele
+    fseek
+    fgets
+    Rotacionar como?
+    
+    **/
 }
