e8e40140c9a94632e541fc44cec15422da905f93
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-06-15 17:31:26 -0300

    Trying to read bin file

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 
 int main(){
 
-    FILE *aquivo
-    if((arquivo=fopen("arquivo.bin", "r") == NULL){
-        printf("Erro ao abrir o aquivo\n");
-        return 1;
+    FILE *arquivo;
+    double Vet[100];
+    int result;
+    int i;
+    
+    arquivo = fopen("teste", "r+");
+     
+    if (arquivo == NULL){
+        printf("Problemas na abertura do arquivo \n");
+        return(0);
     }
 
-    return 0;
+    result = fread (&Vet[0], sizeof(int), 100, arquivo);
+    printf("Nro de elementos lidos: %d\n", result);
+
+    for (i=0; i<result; i++)
+    printf("%lf\n", Vet[i]);
+  
+    fclose(arquivo);
 }
