f9aa331082f67ca1a059f7d5ed07f5ae033ade83
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-06-15 16:39:16 -0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    FILE *aquivo
+    if((arquivo=fopen("arquivo.bin", "r") == NULL){
+        printf("Erro ao abrir o aquivo\n");
+        return 1;
+    }
+
+    return 0;
+}
